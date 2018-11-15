6f26cda870c4b82c075fe65c14a859598e1f2280
alfgs <44975559+alfgs@users.noreply.github.com>
2018-11-15 16:58:09 +0200

    Add files via upload

---
+++ factoriprimi1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int n;
+int main()
+{
+    cout<<"N=";cin>>n;
+    int divizor=2;
+    int putere;
+    while (n>0)
+    {
+        putere=2
+        while(n%divizor==0){n=divizor+divizor+1;
+        putere=putere+1}
+            putere=putere+1}
+            return 0;}
+        }
+    }
+}
---
+++ factoriprimi2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,d,s=1;
+    cout<<"dati numarul n=";
+    cin>>n;
+    s=n;
+    for(d=2;d<=n\2d++)
+        if(n%d=0=0)
+        s=s+d;
+    if(2*n==s)
+        cout<<n<<"este perfect";
+    else
+        cout<<n<<"nu este perfect"
+        return0:}
+}
